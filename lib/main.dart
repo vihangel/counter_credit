c91e52d4298ee3370c91c2c2d3c4fe9dc4e69109
Angel <vitoria.angel2002@gmail.com>
2024-05-19 21:22:28 -0400

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:counter_credit/firebase_options.dart';
 import 'package:counter_credit/routes/setup_routes.dart';
 import 'package:counter_credit/screens/admin/notify_product_listener.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -9,6 +11,9 @@ void main() {
     url: 'https://ewgwmdmqzgdumpxjohac.supabase.co',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3Z3dtZG1xemdkdW1weGpvaGFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4OTA5OTYsImV4cCI6MjAzMTQ2Njk5Nn0.APbCJc1yAnSRIMW2CkKrh-KuvyxfCn3lfgTl0_V7ZY0',
+  );
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(
